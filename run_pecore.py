1269436ed1f0fb04ad7cc8cbd853bb25d04d2048
biacraciun <b.craciun@student.rug.nl>
2024-03-28 16:49:33 +0100

    Minor fix

--- run_pecore.py
+++ run_pecore.py
@@ -9,7 +9,8 @@ def run_pecore(langugage_code):
     data = setup_pecore()
     # for i in range(len(data)):
     #     use_pecore(data["sentence_eng_Latn"][i], data["sentence_context_eng_Latn"][i], data["sentence_context_" + langugage_code][i], langugage_code)
-    use_pecore(data["sentence_eng_Latn"][0], data["sentence_context_eng_Latn"][0], data["sentence_context_" + langugage_code][0], langugage_code)
+    use_pecore(data["sentence_eng_Latn"][1], data["sentence_context_eng_Latn"][1], data["sentence_context_" + langugage_code][1], langugage_code)
 
 if __name__ == "__main__":
+    # langugage_codes = ['bul_Cyrl', 'nld_Latn', 'ron_Latn']
     run_pecore(langugage_code='ron_Latn')
