c7f4ca8f5baae6103a7de33b2a980fe2a6f26d3c
biacraciun <b.craciun@student.rug.nl>
2024-04-03 12:36:45 +0200

    Small fix for running pecore in notebook.

--- modules/run_pecore.py
+++ modules/run_pecore.py
@@ -3,25 +3,31 @@ from use_pecore import use_pecore, load_model
 from datasets import load_from_disk
 import os
 
-def run_pecore(langugage_code):
+def run_pecore(langugage_code, notebook=False):
     """
     This function runs the PECORE model on the data after filtering it.
     Args:
         langugage_code (str): The language code of the target language.
+        notebook (bool): Whether the code is running in a notebook environment.
     Returns:
         None
     """
     
-    # Set up the data for the PECORE model if it does not exist
-    if not os.path.exists("data/filtered_data_with_context"):
-        data = setup_pecore()
-        
-    # Load the data if it exists
+    # Load the data if running in a notebook environment
+    if notebook:
+        data = load_from_disk("NLP---Understanding-Context-Usage-in-Machine-Translation/data/filtered_data_with_context")
+    
     else:
-        data = load_from_disk("data/filtered_data_with_context") 
-        
-    # Load the model speecific to the target language
-    model = load_model(langugage_code)
+        # Set up the data for the PECORE model if it does not exist
+        if not os.path.exists("data/filtered_data_with_context"):
+            data = setup_pecore()
+            
+        # Load the data if it exists
+        else:
+            data = load_from_disk("data/filtered_data_with_context") 
+            
+        # Load the model speecific to the target language
+        model = load_model(langugage_code)
     
     # Create the output directory per language if it does not exist
     directory_path = os.path.join("pecore_output", langugage_code)
@@ -50,7 +56,7 @@ if __name__ == "__main__":
     if not os.path.exists("pecore_output"):
        os.makedirs("pecore_output")
     
-    run_pecore('ron_Latn')
+    run_pecore('ron_Latn', notebook=False)
     
 
     
