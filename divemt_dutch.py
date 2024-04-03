8b58a43bed1a7c330db48468987f9d55a45914ef
S4410653 <sijbren.vv@outlook.com>
2024-04-03 14:42:46 +0200

    Update divemt_dutch.py: Also compute PECoRe context attribution if context is present

--- divemt_dutch.py
+++ divemt_dutch.py
@@ -4,8 +4,9 @@ import sys
 from transformers import (
     set_seed
 )
-import inseq
 import os
+import inseq
+from inseq.commands.attribute_context.attribute_context import attribute_context_with_model, AttributeContextArgs
 
 
 def load_inseq_model(model_name, gradient):
@@ -19,9 +20,50 @@ def load_inseq_model(model_name, gradient):
     )
 
 
+def get_pecore_args(sentence_eng_Latn, context_eng_Latn, context_target_language,
+               sub_directory_name):
+    """
+    This function uses the PECORE model to attribute the context of a sentence in a target language.
+    The code was adapted from the PECORE demo. https://huggingface.co/spaces/gsarti/pecore
+    Args:
+        sentence_eng_Latn (str): The current sentence in English.
+        context_eng_Latn (str): The context in English.
+        context_target_language (str): The context in the target language.
+        sub_directory_name (path): The subdirectory to save the files to.
+        (global variable) tgt_lang (str): The language code of the target language.
+    Returns:
+        The PECoRe args
+    """
+    # create directory for saving the output
+
+    # Set the arguments for the PECoRe model
+    pecore_args = AttributeContextArgs(
+        model_name_or_path=args.model,
+        attribution_method=args.gradient,
+        attributed_fn="probability",
+        context_sensitivity_metric="kl_divergence",
+        context_sensitivity_std_threshold=0,
+        attribution_std_threshold=2,
+        attribution_topk=5,
+        input_current_text=sentence_eng_Latn,
+        input_context_text=context_eng_Latn,
+        contextless_input_current_text="""{current}""",
+        input_template="""{context} {current}""",
+        output_context_text=context_target_language,
+        contextless_output_current_text="""{current}""",
+        output_template="{context} {current}",
+        save_path=sub_directory_name + "_pec.json",
+        viz_path=sub_directory_name + "_pec.html",
+        tokenizer_kwargs={'src_lang': 'eng_Latn', 'tgt_lang': tgt_lang},
+    )
+
+    return pecore_args
+
+
 def comp_sal_scores(example, idx):
     """ This function calculates the salient (importance) score between \
     two provided sentences and outputs the HTML file to a prefixed folder. \
+    Additionally, it outputs the PECoRE context attribution to the same prefixed folder. \
     Moreover, it outputs the modified words during post-editing for further analysis."""
 
     # Get attributes
@@ -53,6 +95,19 @@ def comp_sal_scores(example, idx):
     words_to_analyse = [example["mt_tokens"][c] for c, tag in enumerate(example["mt_wmt22_qe"]) if tag == "BAD"]
     with open(f"divemt_dutch_analyse/sen_{idx + 1}/sen_{idx + 1}.txt", "w") as f:
         f.write(f"Source sen: {example['src_text']} \nMT sen: {example['mt_text']} \nTarget sen: {example['tgt_text']} \nModified words during post-editing: {str(words_to_analyse)}")
+
+    # Run PECoRe and output the overall context attribution process
+    try:
+        pecore_args = get_pecore_args(
+            sentence_eng_Latn=example["src_text"],
+            context_eng_Latn=example["src_context"],
+            context_target_language=example["tgt_context"],
+            sub_directory_name=f"divemt_dutch_analyse/sen_{idx + 1}/sen_{idx + 1}")
+
+        return attribute_context_with_model(pecore_args, model)
+    except ValueError:
+        print(f"Sentence {idx + 1} (example {idx}) has no context as it is the first sentence. "
+              f"Hence, PECoRe cannot be utilised.", file=sys.stderr)
 
 
 if __name__ == "__main__":
