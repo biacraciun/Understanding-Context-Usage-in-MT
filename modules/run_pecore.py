b676bd446bdfb957025377d7c640590f06410963
biacraciun <b.craciun@student.rug.nl>
2024-04-05 17:50:01 +0200

    Added exact indices for analysis

--- modules/run_pecore.py
+++ modules/run_pecore.py
@@ -2,6 +2,7 @@ from setup_data_pecore import setup_pecore, use_pecore
 from use_pecore import use_pecore, load_model
 from datasets import load_from_disk
 import os
+import sys
 
 def run_pecore(langugage_code, notebook=False):
     """
@@ -40,13 +41,21 @@ def run_pecore(langugage_code, notebook=False):
        os.makedirs(directory_path)
 
     # Indices of the examples to run the PECORE model on
-    indices = [12, 18, 25, 29, 39, 53]
+
+    if langugage_code == 'ron_Latn':
+        indices = [12, 18, 25, 29, 39, 53, 37, 44, 1, 2, 16]
+    elif langugage_code == 'bul_Cyrl':
+        indices = [12, 18, 25, 29, 37, 39, 0, 3, 44, 53, 60]
+    elif langugage_code == 'nld_Latn':
+        indices = [12, 18, 25, 29, 39, 53, 1, 2, 3, 7, 16]
     
     # Run the PECORE model for the target language 
     for i in indices:
         
         # Create the sub-directory for saving the output 
         sub_directory_name = os.path.join(directory_path, "example_" + str(i))
+        
+        print(f"Sentence {i}", file=sys.stderr)
         
         use_pecore(data["sentence_eng_Latn"][i], data["sentence_context_eng_Latn"][i], data["sentence_context_" + langugage_code][i], langugage_code, model, sub_directory_name)
         
